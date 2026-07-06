408c3662b47180e6a10af590713465d06c74cf15
AryaHanaafi <aryahanaafi115@gmail.com>
2026-07-07 02:12:22 +0700

    Auto-create superuser on deploy

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ set -o errexit
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+# Buat akun admin otomatis jika belum ada
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
+
 exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8080}
